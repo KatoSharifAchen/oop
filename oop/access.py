258d0773d6b540e832a744fc2675a014c2665f3b
Kato Sharif Achen <sharifachenkato@gmail.com>
2025-10-01 22:20:20 +0300

    Add files via upload

---
+++ oop/access.py
@@ -0,0 +1,9 @@
+class students:
+    def __init__(self,name):
+        self.name=name
+        self.cgpa=3.5
+        self.__password="1234"
+    
+students = students ('Kato')
+print (students.name)
+print(students.cgpa)
