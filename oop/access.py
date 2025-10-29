13a207cbc802a04d73f7b85c10bd45e945e32a88
Kato Sharif Achen <sharifachenkato@gmail.com>
2025-10-29 14:15:45 +0300

    Update access.py

--- oop/access.py
+++ oop/access.py
@@ -2,7 +2,7 @@ class students:
     def __init__(self,name):
         self.name=name
         self.cgpa=3.5
-        self.__password="1234"
+        self.__password="123456"
     
 students = students ('Sharifs')
 print (students.name)
